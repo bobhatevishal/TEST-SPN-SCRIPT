7f1fb6eb85b423763db58ee7b453390c8e673545
Bobhate Vishal <bobhatev13@gmail.com>
2026-02-18 10:08:27 +0530

    Update create_oauth_secret.sh

--- scripts/create_oauth_secret.sh
+++ scripts/create_oauth_secret.sh
@@ -11,10 +11,10 @@ else
 fi
 
 # Validation
-check_var "TARGET_SPN_DISPLAY_NAME"
-check_var "DATABRICKS_INTERNAL_ID"
-check_var "DATABRICKS_TOKEN"
-check_var "ACCOUNT_ID"
+# check_var "TARGET_SPN_DISPLAY_NAME"
+# check_var "DATABRICKS_INTERNAL_ID"
+# check_var "DATABRICKS_TOKEN"
+# check_var "ACCOUNT_ID"
 
 log_info "Creating OAuth Secret for: $TARGET_SPN_DISPLAY_NAME ($DATABRICKS_INTERNAL_ID)"
 
